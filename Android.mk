f5295ec948e78ad0ab414fc605c965c931cce5ab
xyzuan <i.xyzuan@gmail.com>
2021-04-02 21:00:12 +0700

    lavender: initial decommon xd. lavender vendor blobs.

--- Android.mk
+++ Android.mk
@@ -4,6 +4,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter clover jasmine_sprout jason lavender platina tulip wayne whyred,$(TARGET_DEVICE)),)
+ifneq ($(filter lavender,$(TARGET_DEVICE)),)
 
 endif
